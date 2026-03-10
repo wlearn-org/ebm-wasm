c71c5c8ec10b889f0cc083fdd1056af996e476c9
zemlyansky <anton.zemlyansky@gmail.com>
2026-03-10 05:42:23 +0000

    refactor: wrap EBMModel with createModelClass for unified task detection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const { loadEBM, getWasm } = require('./wasm.js')
-const { EBMModel } = require('./model.js')
+const { EBMModel: EBMModelImpl } = require('./model.js')
+const { createModelClass } = require('@wlearn/core')
+
+const EBMModel = createModelClass(EBMModelImpl, EBMModelImpl, { name: 'EBMModel', load: loadEBM })
 
 // Convenience: create, fit, return fitted model
 async function train(params, X, y) {
   const model = await EBMModel.create(params)
-  model.fit(X, y)
+  await model.fit(X, y)
   return model
 }
 
